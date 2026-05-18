df71ee3588a058cdaa0222f255ffa4d492904949
JPerez1-jpg <juanperezduthu.et36@gmail.com>
2026-05-18 17:55:49 -0300

    Update ping.bat

--- ping.bat
+++ ping.bat
@@ -1,6 +1,7 @@
 @echo off
 color 0B
 
+
 :Menu
 echo 1. Verificacion de Bucle Local
 echo 2. Obtener IP de la placa
@@ -9,7 +10,10 @@ echo 4. Monitoreo Continuo
 echo 5. Definir Cantidad de Paquetes
 echo 6. Prueba de Carga
 echo 7. Resolucion de Nombres
-set /p opcion=Elegi una opcion (1-7)
+echo 8. Tracert
+echo 9. Nslookup
+echo 10. Ipconfig
+set /p opcion=Elegi una opcion (1-10)
 
 if %opcion%==1 goto loopback
 if %opcion%==2 goto IPPlaca
@@ -18,12 +22,15 @@ if %opcion%==4 goto Monitoreo
 if %opcion%==5 goto DefCPaq
 if %opcion%==6 goto PCarga
 if %opcion%==7 goto ReNomb
+if %opcion%==8 goto VCamm
+if %opcion%==9 goto PrDNS
+if %opcion%==10 goto InfDis
+
 
 :loopback
 ping 127.0.0.1
 pause
 goto Menu
-
 
 :IPPlaca
 echo Mostrar informacion de Red
@@ -32,7 +39,6 @@ pause
 goto Menu
 
 :PConect
-cls
 set /p url= Ingrese la url a testear:
 echo Enviando 4 paquetes a %url%...
 ping %url%
@@ -40,7 +46,6 @@ pause
 goto Menu
 
 :Monitoreo
-cls
 set /p host= Ingrese la url o ip a monitorear:
 echo Iniciando monitoreo continuo a %host%
 ping -t %host%
@@ -48,7 +53,6 @@ pause
 goto Menu
 
 :DefCPaq
-cls
 set /p host= Ingrese la URL o IP del destino:
 echo Enviando 10 paquetes a %host%
 echo Obteniendo promedio de latencia
@@ -57,7 +61,6 @@ pause
 goto Menu
 
 :PCarga
-cls
 set /p host= Ingrese la url o ip del destino:
 echo Enviando paquetes de 1000 bytes a %host%
 ping -l 1000 %host%
@@ -65,8 +68,25 @@ pause
 goto Menu
 
 :ReNomb
-cls
 set /p ip= Ingrese la direccion IP:
 ping -a %ip%
+pause
+goto Menu
+
+:VCamm
+echo Mostrando configuracion detallada de red...
+ipconfig /all
+pause
+goto Menu
+
+:PrDNS
+set /p dominio=Ingrese el dominio o IP para consultar DNS (ej: google.com):
+nslookup %dominio%
+pause
+goto Menu
+
+:InfDis
+set /p destino=Ingrese la URL o IP para rastrear la ruta:
+tracert %destino%
 pause
 goto Menu
